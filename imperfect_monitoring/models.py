1cc16642254b714c89bd812e0a3e016bd67277be
Tina Peng <tina2015p@gmail.com>
2018-05-01 12:10:47 -0700

    cleaned up code

--- imperfect_monitoring/models.py
+++ imperfect_monitoring/models.py
@@ -131,7 +131,6 @@ class Group(DecisionGroup):
         # someone will forget this and get very confused when the tick functions use stale data.
         self.refresh_from_db()
         msg = {}
-        print(self.group_decisions)
         
         if self.state == 'results':
             msg = {
@@ -224,7 +223,6 @@ class Group(DecisionGroup):
     def _on_decisions_event(self, event=None, **kwargs):
         super()._on_decisions_event(event, **kwargs)
         if event:
-            print('decision event value: ', event.value)
             self.save()
 
 
